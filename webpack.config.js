eaa3e2120e01f3e356961a91b55874ef994ab030
myguddy <seungil.han@gmai.com>
2016-09-21 11:22:49 +0900

    UI  개발을 위한 실행설정 변경

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,50 @@
-var path = require('path');
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
+
     entry: [
         'babel-polyfill',
         './src/index.js',
+        'webpack/hot/only-dev-server',
         './src/style.css'
     ],
 
     output: {
-        path: __dirname + '/public/',
+        path: '/',
         filename: 'bundle.js'
     },
+
+    devServer: {
+        hot: true,
+        filename: 'bundle.js',
+        publicPath: '/',
+        historyApiFallback: true,
+        contentBase: './public',
+        host: '0.0.0.0',
+        port: 5000,
+        stats: {
+          // Config for minimal console.log mess.
+          assets: false,
+          colors: true,
+          version: false,
+          hash: false,
+          timings: false,
+          chunks: false,
+          chunkModules: false
+        }
+    },
+
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
 
     module: {
         loaders: [
             {
                 test: /\.js$/,
-                loaders: ['babel?' + JSON.stringify({
+                loaders: ['react-hot', 'babel?' + JSON.stringify({
                     cacheDirectory: true,
                     presets: ['es2015', 'react']
                 })],
@@ -32,19 +59,7 @@ module.exports = {
 
     resolve: {
         root: path.resolve('./src')
-    },
+    }
 
-    plugins:[
-        new webpack.DefinePlugin({
-          'process.env':{
-            'NODE_ENV': JSON.stringify('production')
-          }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-          compress:{
-            warnings: true
-          }
-        })
-    ]
 
 };
